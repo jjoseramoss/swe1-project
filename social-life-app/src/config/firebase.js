6e450f8d64072389d807588d194ae636f8371a05
Hector Corpus <hectorcorpus121505@gmail.com>
2025-10-17 14:29:37 -0500

    firebase config file

--- social-life-app/src/config/firebase.js
+++ social-life-app/src/config/firebase.js
@@ -7,13 +7,13 @@ import { initializeApp } from "firebase/app";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 //this config is public data so it doesn't need to be added to a gitignore :)
 const firebaseConfig = {
-  apiKey: "AIzaSyA_Ff7Zx3m_0Nr1BArcVE_9C2pHjOJkvAE",
-  authDomain: "swe1-project.firebaseapp.com",
-  projectId: "swe1-project",
-  storageBucket: "swe1-project.firebasestorage.app",
-  messagingSenderId: "595132676573",
-  appId: "1:595132676573:web:ffd9130a6a5b20f4b1de34",
-  measurementId: "G-M9LS0MHP3Q"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
