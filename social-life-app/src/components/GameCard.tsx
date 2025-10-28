b84cffee494393073457b0863f565c897253f8c4
Jose Ramos <144376748+jjoseramoss@users.noreply.github.com>
2025-10-28 08:41:50 -0500

    added basic frontend setup

---
+++ social-life-app/src/components/GameCard.tsx
@@ -0,0 +1,25 @@
+
+
+const GameCard = ({title, description, photo}) => {
+  return (
+    <div className="card bg-base-100 w-96 shadow-sm">
+      <figure>
+        <img
+          src={photo}
+          alt="game image"
+        />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title">Who Knows Me? - {title}</h2>
+        <p>
+          {description}
+        </p>
+        <div className="card-actions justify-end">
+          <button className="btn btn-primary">Play</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default GameCard;
