8b39dfe4284f60121a796c0640dbb2f14777465e
Hector Corpus <hectorcorpus121505@gmail.com>
2025-11-10 14:41:34 -0600

    Linked all webpages together

--- social-life-app/src/pages/Login.tsx
+++ social-life-app/src/pages/Login.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
   return (
@@ -39,9 +40,12 @@ const Login = () => {
             </a>
           </div>
 
-          <button type="submit" className="btn btn-primary w-full">
-            Login
-          </button>
+          <Link 
+          to="/games" 
+          type="submit" 
+          className="btn btn-primary w-full">
+          Login
+          </Link>
 
           <button type="button" className="btn btn-outline w-full">
             Sign in with Google
